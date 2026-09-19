2b0c91297da411ad8c285129dd3c9ce8a8a4b9d7
yitang1 <53383908+yitang1@users.noreply.github.com>
2026-09-19 20:34:18 +0800

    实现弱引用的前提下，去调用其他模组的字段属性方法
    头皮发麻的弱引用

---
+++ WeakReferences/CalamityMod/WRCalamityHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod;
+using CalamityMod.CalPlayer;
+
+namespace LaziestNPC.WeakReferences.CalamityMod
+{
+    [JITWhenModsEnabled("CalamityMod")]
+    public static class WRCalamityHelper
+    {
+        public static void ApplyCalamityPenumbraPotion(Player player)
+        {
+            CalamityPlayer calamityPlayer = player.Calamity();
+
+            if (Main.eclipse || calamityPlayer.umbraphileSet)
+            {
+                calamityPlayer.stealthGenStandstill += 0.2f;
+                calamityPlayer.stealthGenMoving += 0.2f;
+            }
+            else
+            {
+                calamityPlayer.stealthGenStandstill += 0.15f;
+                calamityPlayer.stealthGenMoving += 0.15f;
+            }
+        }
+    }
+}
---
+++ WeakReferences/CalamityMod/WRCalamityPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod;
+using CalamityMod.CalPlayer;
+using CalamityMod.Items.Weapons.Rogue;
+using LaziestNPC.Content.Buffs.Potions;
+using LaziestNPC.Common.Helpers;
+using LaziestNPC.WeakReferences.CalamityMod;
+
+namespace LaziestNPC.Globals.GlobalMods.WeakReferences.CalamityMod
+{
+    public class WRCalamityPlayer : ModPlayer
+    { 
+        public override void PostUpdateMiscEffects()
+        {
+            if (!Player.LaziestNPC().penumbra)
+                return;
+
+            if (!ModLoader.HasMod("CalamityMod"))
+                return;
+
+            WRCalamityHelper.ApplyCalamityPenumbraPotion(Player);
+        }
+    }
+}
